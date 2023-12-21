57e265b21e0d783354fcbf57dddc069e435b1d33
Muluken <mulukeneshetu70@gmail.com>
2023-12-21 05:26:16 -0800

    RSA-Factoring-Challenge

--- factors.py
+++ factors.py
@@ -1,23 +1,29 @@
+#!/usr/bin/env python
+
+import subprocess
 import sys
 
-def factorize(number):
-    for i in range(2, number):
-        if number % i == 0:
-            return i, number // i
-    return None, None
+def check_factor(num1, num2, num3):
+    if num3 is None:
+        product = num1 * num2
+    else:
+        product = num3
 
-def main():
+    num = f"{num1}={num2}*{product}"
+    print(num)
+
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: factors <file>")
-        return
+        print('Usage: factors <file>')
+        sys.exit(1)
 
     input_file = sys.argv[1]
     with open(input_file, 'r') as file:
         for line in file:
             number = int(line)
-            factor1, factor2 = factorize(number)
-            print(f"{number}={factor1}*{factor2}")
-
-if __name__ == "__main__":
-    main()
-
+            factor_output = subprocess.check_output(["factor", str(number)]).decode("utf-8").strip()
+            factors = list(map(int, factor_output.split(":")[1].split()))
+            if len(factors) == 2:
+                check_factor(number, factors[0], factors[1])
+            else:
+                check_factor(number, factors[0], None)
